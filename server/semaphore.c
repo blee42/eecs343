d83028b38dfb4c077572bf198a1eb05c2d88aa27
Brittany Lee <BrittanyLee2015@u.northwestern.edu>
2014-11-17 01:31:01 -0600

    added semaphore code

--- server/semaphore.c
+++ server/semaphore.c
@@ -4,20 +4,47 @@
 
 
 typedef struct m_sem_t {
-    int value;
+    pthread_mutex_t mutex;
+    pthread_cond_t cond;
+    int count;
 } m_sem_t;
 
+int sem_init(m_sem_t *s);
 int sem_wait(m_sem_t *s);
 int sem_post(m_sem_t *s);
 
+int sem_init(m_sem_t *s)
+{	
+	int* count = &s->count;
+	pthread_mutex_init(&s->mutex, NULL);
+	pthread_cond_init(&s->cond, NULL);
+	count = 0;
+	return 0;
+}
+
 int sem_wait(m_sem_t *s)
 {
-    //TODO
+	pthread_mutex_t* mutex = &s->mutex;
+	int* count = &s->count;
+
+    pthread_mutex_lock(mutex);
+    while (count <= 0)
+    {
+    	pthread_cond_wait(&s->cond, mutex);
+    }
+    count--;
+    pthread_mutex_unlock(mutex);
     return 0;
 }
 
 int sem_post(m_sem_t *s)
 {
-    //TODO
+    pthread_mutex_t* mutex = &s->mutex;
+    int* count = &s->count;
+
+    pthread_mutex_lock(mutex);
+   	count++;
+    pthread_cond_broadcast(&s->cond);
+    pthread_mutex_unlock(mutex);
     return 0;
 }
