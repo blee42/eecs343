e7e3b8e92e6bd8f9b687279ed1a49691a167cf62
aspin <tsunami70875@gmail.com>
2014-11-17 22:49:37 -0600

    fixed a warning

--- server/semaphore.c
+++ server/semaphore.c
@@ -15,24 +15,22 @@ int sem_post(m_sem_t *s);
 
 int sem_init(m_sem_t *s)
 {	
-	int* count = &s->count;
-	pthread_mutex_init(&s->mutex, NULL);
-	pthread_cond_init(&s->cond, NULL);
-	count = 0;
+    pthread_mutex_init(&s->mutex, NULL);
+    pthread_cond_init(&s->cond, NULL);
+	s->count = 0;
 	return 0;
 }
 
 int sem_wait(m_sem_t *s)
 {
 	pthread_mutex_t* mutex = &s->mutex;
-	int* count = &s->count;
 
     pthread_mutex_lock(mutex);
-    while (count <= 0)
+    while (s->count <= 0)
     {
     	pthread_cond_wait(&s->cond, mutex);
     }
-    count--;
+    s->count--;
     pthread_mutex_unlock(mutex);
     return 0;
 }
@@ -40,10 +38,9 @@ int sem_wait(m_sem_t *s)
 int sem_post(m_sem_t *s)
 {
     pthread_mutex_t* mutex = &s->mutex;
-    int* count = &s->count;
 
     pthread_mutex_lock(mutex);
-   	count++;
+   	s->count++;
     pthread_cond_broadcast(&s->cond);
     pthread_mutex_unlock(mutex);
     return 0;
